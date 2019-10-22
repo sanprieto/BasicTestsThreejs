9bab151fdd6d3d65f053dfbbbaf3b5cfaf0286c7
Sanprieto <ricardosanprieto@gmail.com>
2019-10-22 20:14:40 +0200

    equalizer sphere noise

---
+++ Equalizer/equalizer Sphere noise/js/sceneControls.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+import OrbitControls from 'three-orbitcontrols';
+
+function createOrbitControls( cam, container ) { 
+
+  const controls = new OrbitControls( cam, container );
+
+}
+
+export { createOrbitControls };
